8cdb43be3b32c9038741bafb3485ce268448cbee
cirickenan-beep <cirickenan@gmail.com>
2026-05-28 17:24:52 +0200

    Update Stramica.py

--- Stramica.py
+++ Stramica.py
@@ -1,18 +1,23 @@
 import streamlit as st
+
 st.title("Moj BH Telecom")
-# 1.
+
+# 1. Kreiramo stanje u memoriji ako već ne postoji
 if "kliknuto" not in st.session_state:
- st.session_state.kliknuto = False
-#2.
- if not st.session_state.kliknuto:
-  Dalje = st.button ("Zaroni u svijet optièkog inteneta")
-  st.session_state.kliknuto = True
-  st.rerun()
-if session_state.kliknuto:
- if Dalje:
-  st.title("Paketi")
-  Ponuda_1 = st.button("20mbps/5 , 25,50KM")
-  Ponuda_2 = st.button("40mbps/10, 39.99KM")
-  Ponuda_3 = st.button("100mbps/30, 69.99KM")
-  Ponuda_4 = st.button("200mbps/60, 90.99KM")
+    st.session_state.kliknuto = False
+
+# 2. Ako dugme NIJE kliknuto, prikaži ga na ekranu
+if not st.session_state.kliknuto:
+    Dalje = st.button("Zaroni u svijet optičkog inteneta")
+    if Dalje:
+        st.session_state.kliknuto = True
+        st.rerun()  
+
+# 3. Ako JE kliknuto, prikaži samo pakete (dugme se sklanja)
+if st.session_state.kliknuto:
+    st.title("Paketi")
+    Ponuda_1 = st.button("20mbps/5 , 25,50KM")
+    Ponuda_2 = st.button("40mbps/10, 39.99KM")
+    Ponuda_3 = st.button("100mbps/30, 69.99KM")
+    Ponuda_4 = st.button("200mbps/60, 90.99KM")
 
