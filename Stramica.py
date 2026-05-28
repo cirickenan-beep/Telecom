4af96f0bbb47d3b10e5758670f5b3a642bf9d838
cirickenan-beep <cirickenan@gmail.com>
2026-05-28 17:14:01 +0200

    Update Stramica.py

--- Stramica.py
+++ Stramica.py
@@ -10,9 +10,9 @@ st.session_state.kliknuto = True
 st.rerurn()
 if session_state.kliknuto:
  if Dalje:
- st.title("Paketi")
- Ponuda_1 = st.button("20mbps/5 , 25,50KM")
- Ponuda_2 = st.button("40mbps/10, 39.99KM")
- Ponuda_3 = st.button("100mbps/30, 69.99KM")
- Ponuda_4 = st.button("200mbps/60, 90.99KM")
+  st.title("Paketi")
+  Ponuda_1 = st.button("20mbps/5 , 25,50KM")
+  Ponuda_2 = st.button("40mbps/10, 39.99KM")
+  Ponuda_3 = st.button("100mbps/30, 69.99KM")
+  Ponuda_4 = st.button("200mbps/60, 90.99KM")
 
