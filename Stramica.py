3fcbe01a376a19b3d61b1667d5f73c37573b70bd
cirickenan-beep <cirickenan@gmail.com>
2026-05-28 17:09:23 +0200

    Update Stramica.py

--- Stramica.py
+++ Stramica.py
@@ -1,12 +1,18 @@
 import streamlit as st
 st.title("Moj BH Telecom")
-Dalje = st.button ("Zaroni u svijet optièkog inteneta")
-if Dalje:
-    st.title("Paketi")
-    Ponuda_1 = st.button("20mbps/5 , 25,50KM")
-    Ponuda_2 = st.button("40mbps/10, 39.99KM")
-    Ponuda_3 = st.button("100mbps/30, 69.99KM")
-    Ponuda_4 = st.button("200mbps/60, 90.99KM")
-
-                     
+# 1.
+if "kliknuto" not in st.session_state:
+ st.session_state.kliknuto = False
+#2.
+ if not st.session_state.kliknuto:
+Dalje = st.button ("Zroni u svijet optièkog inteneta")
+st.session_state.kliknuto = True
+st.return()
+if session_state.kliknuto:
+ if Dalje:
+ st.title("Paketi")
+ Ponuda_1 = st.button("20mbps/5 , 25,50KM")
+ Ponuda_2 = st.button("40mbps/10, 39.99KM")
+ Ponuda_3 = st.button("100mbps/30, 69.99KM")
+ Ponuda_4 = st.button("200mbps/60, 90.99KM")
 
