3738ff08cdb684b731ac0ff5b667e27f886804f1
cirickenan-beep <cirickenan@gmail.com>
2026-05-28 18:32:11 +0200

    Update Stramica.py

--- Stramica.py
+++ Stramica.py
@@ -1,4 +1,8 @@
-import streamlit as st
+mport streamlit as st
+
+# Sve je spakovano unutar zagrada i provjereno radi u IDLE-u
+st.markdown('<style>[data-testid="stAppViewContainer"] {background-color: #FF6600 !important;} h1, h2, p, div, span, label {color: black !important;}</style>', unsafe_allow_html=True)
+
 st.title("Moj BH Telecom")
 
 # 1. Kreiramo stanje u memoriji ako već ne postoji
@@ -12,11 +16,10 @@ if not st.session_state.kliknuto:
         st.session_state.kliknuto = True
         st.rerun()  
 
-# 3. Ako JE kliknuto, prikaži samo pakete (dugme se sklanja)
+# 3. Ako JE kliknuto, prikaži samo pakete
 if st.session_state.kliknuto:
     st.title("Paketi")
     Ponuda_1 = st.button("20mbps/5 , 25,50KM")
     Ponuda_2 = st.button("40mbps/10, 39.99KM")
     Ponuda_3 = st.button("100mbps/30, 69.99KM")
     Ponuda_4 = st.button("200mbps/60, 90.99KM")
-
