43553684c4c4a9453ee76b459398bb678e0348e1
cirickenan-beep <cirickenan@gmail.com>
2026-05-28 12:11:16 +0200

    Add files via upload

--- Stramica.py
+++ Stramica.py
@@ -1,6 +1,12 @@
 import streamlit as st
-
 st.title("Moj BH Telecom")
-st.button ("Zroni u svijet optièkog inteneta")
-st.write("Paketi")
-st.button("20mbps/5 , 25,50KM")
+Dalje = st.button ("Zroni u svijet optièkog inteneta")
+if Dalje
+st.title("Paketi")
+Ponuda_1 = st.button("20mbps/5 , 25,50KM")
+Ponuda_2 = st.button("40mbps/10, 39.99KM")
+Ponuda_3 = st.button("100mbps/30, 69.99KM")
+Ponuda_4 = st.button("200mbps/60, 90.99KM")
+
+                     
+
